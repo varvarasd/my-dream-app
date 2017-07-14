679e8d6d5a84941adb55336b84882a8877290965
Daniel Varvara <varvara_sd@yahoo.co.uk>
2017-07-14 16:23:30 +0300

    show use of template and multi line html in new-component.component.ts

--- src/app/new-component/new-component.component.ts
+++ src/app/new-component/new-component.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-new-component',
-  templateUrl: './new-component.component.html',
+  template: `
+    <h1> This is a <code>html</code> template </h1>
+    <h2> As a multi line example </h2>
+  `,
   styleUrls: ['./new-component.component.css']
 })
 export class NewComponentComponent implements OnInit {
