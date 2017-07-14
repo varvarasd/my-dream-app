b4aff131e73e7ad82c745e9ca3796362b1edb314
Daniel Varvara <varvara_sd@yahoo.co.uk>
2017-07-14 17:00:08 +0300

    display dynamic data in the component template (aka interpolation) from the component class (usually)

--- src/app/new-component/new-component.component.ts
+++ src/app/new-component/new-component.component.ts
@@ -3,16 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-new-component',
   template: `
-    <h1> This is a <code>html</code> template </h1>
-    <h2> As a multi line example </h2>
+    <h1> This </h1>
+    <h2>Is a multi line <code>html</code> template </h2>
+    <h2> Which tells us that {{ myObject.name }} is {{ myObject.age }} years old and lives in {{ myObject.location }} </h2>
   `,
   styleUrls: ['./new-component.component.css']
 })
-export class NewComponentComponent implements OnInit {
+export class NewComponentComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  myObject = {
+    name: 'Jose',
+    gender: 'male',
+    age: 33,
+    location: 'UK'
   }
 
 }
