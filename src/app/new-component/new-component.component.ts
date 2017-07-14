c2fdc3ad29941e6891a5db7f6155e93bc77b4c2c
Daniel Varvara <varvara_sd@yahoo.co.uk>
2017-07-14 17:49:49 +0300

    iterate through an array and interpolate to display the data within the <li> (same as how to iterate through an array of objects)

--- src/app/new-component/new-component.component.ts
+++ src/app/new-component/new-component.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-new-component',
   template: `
     <h1> This </h1>
-    <h2>Is a multi line <code>html</code> template </h2>
+    <h2> Is a multi line <code>html</code> template </h2>
     <h2> Which tells us that {{ myObject.name }} is {{ myObject.age }} years old and lives in {{ myObject.location }} </h2>
+    <h2> {{ myObject.name }} likes: </h2>
+    <ul>
+      <li *ngFor="let arrItem of myArr"> {{ arrItem }} </li>
+    </ul>
+
   `,
   styleUrls: ['./new-component.component.css']
 })
@@ -17,5 +22,7 @@ export class NewComponentComponent {
     age: 33,
     location: 'UK'
   }
+
+  myArr = ['movies', 'series', 'online games']
 
 }
