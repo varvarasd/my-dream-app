a13e6f54b464f1b55af0b0f3d3dab7880ce7a222
Daniel Varvara <varvara_sd@yahoo.co.uk>
2017-07-14 18:56:20 +0300

    make use of then and else condition by creating a local variable which is linked to the ng-template

--- src/app/new-component/new-component.component.ts
+++ src/app/new-component/new-component.component.ts
@@ -10,8 +10,16 @@ import { Component } from '@angular/core';
     <ul>
       <li *ngFor="let arrItem of myArr"> {{ arrItem }} </li>
     </ul>
-    <p *ngIf="myArr ; else otherTmpl"> {{myObject.name}} confirms the list!</p>
-    <ng-template #otherTmpl> {{myObject.name}} does not wish to share what he likes!</ng-template>
+    <p *ngIf="myArr ; else otherVar"> {{myObject.name}} confirms the list.</p>
+    <ng-template #otherVar> {{myObject.name}} does not wish to share what he likes!</ng-template>
+    <br>
+    <br>
+    <div *ngIf="myVar; then tempVar1 else tempVar2"></div>
+    <ng-template #tempVar1> {{myObject.name}} thinks that list should do it.</ng-template>
+    <ng-template #tempVar2> {{myObject.name}} would like to go home now.</ng-template>
+
+
+
 
   `,
   styleUrls: ['./new-component.component.css']
@@ -27,5 +35,7 @@ export class NewComponentComponent {
 
   // replace below myArr = false;  to display the ng-template #otherTmpl
   myArr = ['movies', 'series', 'online games'];
+  //-------------------
+  myVar = false;
 
 }
