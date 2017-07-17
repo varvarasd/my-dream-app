c61dca947857f696cbcabf832fb5cbce22500f6e
Daniel Varvara <varvara_sd@yahoo.co.uk>
2017-07-17 16:31:00 +0300

    multi class binding by binding the template expression to the object. add or remove classes with true or false boolean.

--- src/app/class-binding/class-binding.component.ts
+++ src/app/class-binding/class-binding.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   template: `
 
     <h1 [class]="classBinding">Class binding <span [class.green-example]="greenExample">example.</span></h1>
-
+    <h2 [ngClass]="moreClasses">Multi class binding with ngClass directive.</h2>
   `,
   styles: [`
 
@@ -21,12 +21,29 @@ import { Component, OnInit } from '@angular/core';
       color: green;
     }
 
+    .underline-class1 {
+      text-decoration: underline;
+    }
+
+    .large-class2 {
+      font-size: 5em;
+    }
+
+    .blue-class3 {
+      color: blue;
+    }
+
     `]
 })
 export class ClassBindingComponent implements OnInit {
 
   classBinding = 'red-title';
   greenExample = 'green-example';
+  moreClasses = {
+    'underline-class1': true,
+    'large-class2': true,
+    'blue-class3': true
+  }
 
   constructor() { }
 
